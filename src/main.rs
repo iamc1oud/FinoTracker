d7016b30907d6f37553603c6684c53b7816a942f
Ajay Kumar Singh <151016997+SubstantialPackage@users.noreply.github.com>
2024-09-21 18:44:29 +0530

    added: project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Address {
+    city: String,
+    zip: String,
+}
+
+struct Person {
+    name: String,
+    address: Address, // Composing an Address struct inside Person
+}
+
 fn main() {
-    print!("Hello world!");
+    
+
+    let person = Person {
+        name: String::from("Alice"),
+     address:    Address {
+        city: String::from("New York"),
+        zip: String::from("10001"),
+    },
+    };
+
+    println!("{} lives in {}", person.name, person.address.city);
 }
